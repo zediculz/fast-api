8df45b67db50485954859aa9877c4c53c916f744
[zedic] <oluwaseyifunmitan1@gmail.com>
2024-10-11 23:49:10 +0100

    changes

--- index.js
+++ index.js
@@ -2,56 +2,116 @@ import Fastify from 'fastify'
 import fs from 'fs'
 
 const app = Fastify()
+const dataUrl = "./bin/data.json"
 
 //crud
+//read
 app.get("/", (request, reply) => {
+    const query = request.query
+    const id = parseInt(query.id)
+
     fs.readFile("./bin/data.json", "utf8", (err, file) => {
         const datas = JSON.parse(file)
-        reply.send(datas)
-    })
+        if (query.id === undefined) {
+            reply.send({ data: datas, status: true })
+        } else {
+            const datas = JSON.parse(file)
+
+            const data = datas?.filter((d, i) => d?.id === id)[0]
+            if (data === undefined) {
+                reply.send({ status: false, message: "not found" })
+            }
+
+            reply.send({ status: true, data })
+        }
+    }) 
 })
 
+//create
+app.post("/", (request, reply) => {
+    const { name, age } = request.body
 
-app.post("/:id", (request, reply) => {
-    const params = request.params
-    const id = parseInt(params.id)
-
-    fs.readFile("./bin/data.json", "utf8", (err, file) => {
-
+    fs.readFile(dataUrl, "utf8", (err, file) => {
         const datas = JSON.parse(file)
-
-        const data = datas?.filter((d, i) => d?.id === id)[0]
-        if (data === undefined) {
-            reply.send({ status: false, message: "not found" })
+        const randId = Math.floor(Math.random() * 9999999)
+        const data = {
+            id: randId,
+            name,
+            age
         }
 
-        reply.send({status: true, data})
-    })
-})
-
-
-app.put("/:id", (request, reply) => {
-    const params = request.params
-    const id = parseInt(params.id)
-    const body = request.body
-
-    fs.readFile("./bin/data.json", "utf8", (err, file) => {
-
-        const datas = JSON.parse(file)
-        const filteredDatas = datas?.filter((d, i) => d?.id !== id)[0]
-
-        const newData = [{ id, name: body?.name, age: body?.age }]
-        const newDatas = [...newData, filteredDatas]
+        const darr = [data]
+        const newDatas = [...darr, ...datas]
 
         const ds = JSON.stringify(newDatas, null, 2)
-        fs.writeFile("./bin/data.json", ds, "utf8", (err, _) => {
+
+        fs.writeFile(dataUrl, ds, "utf8", (err, _) => {
             if (err) {
                 throw err
             }
 
-            reply.send({ status: true, message: "data updated successfully", data: { id, name: body?.name, age: body?.age } })
+            reply.send({ status: true, message: "data added successfully", data: newDatas })
         })
-       
+    })
+})
+
+//delete
+app.post("/delete/:id", (request, reply) => {
+    const params = request.params
+    const id = parseInt(params?.id)
+
+    fs.readFile(dataUrl, (err, file) => {
+        const datas = JSON.parse(file)
+
+        const exist = datas?.filter((d, i) => {
+            console.log(d)
+            return d?.id === id
+        })
+        console.log(exist)
+
+        if (exist?.length === 1) {
+            const newDatas = datas?.filter((d, i) => d?.id !== id)
+            const ds = JSON.stringify(newDatas, null, 2)
+
+            fs.writeFile(dataUrl, ds, "utf8", (err, _) => {
+                if (err) {
+                    throw err
+                }
+
+                reply.send({ status: true, message: "data removed", id: id })
+            })
+        } else {
+            reply.send({ status: false, message: "data not found" })
+        }
+    })
+})
+
+//update
+app.post("/update/:id", (request, reply) => {
+    const params = request.params
+    const id = parseInt(params.id)
+    const body = request.body
+
+    const { name, age } = body
+
+    fs.readFile(dataUrl, (err, file) => {
+        const datas = JSON.parse(file)
+        const existData = datas?.filter((d, i) => d?.id === id)[0]
+        const nd = { id, name, age }
+        const updateData = { ...existData, ...nd }
+
+        const filterData = datas?.filter((d, i) => d?.id !== id)
+        const newDatas  = [updateData, ...filterData]
+        
+        const ds = JSON.stringify(newDatas, null, 2)
+        
+        fs.writeFile(dataUrl, ds, "utf8", (err, _) => {
+            if (err) {
+                throw err
+            }
+
+            reply.send({ status: true, message: "data updated", id })
+        })
     })
 })
 
@@ -61,5 +121,7 @@ app.listen({ port: 3000, host: "0.0.0.0" }, function (err, address) {
         server.log.error(err)
         process.exit(1)
     }
+
+    console.log(address)
     // Server is now listening on ${address}
 })
